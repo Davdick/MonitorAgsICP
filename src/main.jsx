19031156b4e9068fa0ceb58518edb14cf4d510d1
davdick <dasddj2@gmail.com>
2024-10-18 14:35:59 -0600

    front act

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'; // Asegúrate de que la ruta sea correcta
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
